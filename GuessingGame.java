1cbeedf3286426939143d295d05ced3c83655b82
WMP\epereira <epereira@wmp.com>
2021-02-22 14:41:02 -0600

    adding variables and set up for the game

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,1 +1,25 @@
+import java.util.*;
+/**
+ * Game to allow user to guess random number.
+ *
+ * @author (bing)
+ * @version ()
+ * @source = https://gist.github.com/quanb19/63e4864496992dc4702d0c7bdb01fac1
+ */
+public class guessingGame
+{
 
+  public static void main(String[] args){
+        int rnd; //random
+        int guessInt;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("This is the one and only guessing game");
+       
+        boolean keepGoing = true;
+        boolean playAgain = true;
+        int guessCounter=0;
+        int guess = 0;
+
+}
+}
